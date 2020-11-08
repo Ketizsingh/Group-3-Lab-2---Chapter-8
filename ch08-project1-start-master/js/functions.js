9a7db6b85d9f5d4b8c1b0275c8dc3ac01bea2bc3
Nikhil Rawal <rawal.nikhil29@gmail.com>
2020-11-07 22:49:42 -0500

    shippingCalculate function

--- ch08-project1-start-master/js/functions.js
+++ ch08-project1-start-master/js/functions.js
@@ -11,4 +11,13 @@ function outputCartRow(file, title, quantity, price, total) {
 function totalCalculate(substotal, shipping, tax) {
 	return Number(substotal) + Number(shipping) + Number(tax);
 }
-        
+
+function shippingCalculate(substotal) // calculating shipping cost using if/else statement
+{
+	if (substotal < 1000) {
+		var shipping = '40.00';
+	} else {
+		var shipping = '0.00';
+	}
+	return shipping;
+}
