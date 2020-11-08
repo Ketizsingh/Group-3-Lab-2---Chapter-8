c0c1763126d151a3067464699b4e7962943daff0
Mohit Rawal <mohitrawal3131@gmail.com>
2020-11-07 22:47:54 -0500

    New Function added
    Calculating Total

--- ch08-project1-start-master/js/functions.js
+++ ch08-project1-start-master/js/functions.js
@@ -8,5 +8,7 @@ function outputCartRow(file, title, quantity, price, total) {
 	return '<tr><td><img src="images/' + file + '"></td><td>' + title + '</td><td>' + quantity + '</td><td>$' + price + '</td><td>$' + total + '</td></tr>';
 }
 
-
+function totalCalculate(substotal, shipping, tax) {
+	return Number(substotal) + Number(shipping) + Number(tax);
+}
         
