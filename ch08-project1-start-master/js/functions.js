5f81eb1d7b8e70f1db48ff60d838459989e5acbd
Ketizsingh <Gurpreettechnologies@gmail.com>
2020-11-07 22:40:09 -0500

    2 functions created calculateTotal() & outputCartRow()

--- ch08-project1-start-master/js/functions.js
+++ ch08-project1-start-master/js/functions.js
@@ -1,4 +1,11 @@
 /* define functions here */
 
+function calculateTotal(quantity, price) {
+	return quantity * price;
+}
+
+function outputCartRow(file, title, quantity, price, total) {
+	return '<tr><td><img src="images/' + file + '"></td><td>' + title + '</td><td>' + quantity + '</td><td>$' + price + '</td><td>$' + total + '</td></tr>';
+}
 
         
