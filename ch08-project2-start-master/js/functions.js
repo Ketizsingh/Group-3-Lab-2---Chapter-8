cfe457841f7928f5565f15ef48082427fa3a52e7
Mohit Rawal <mohitrawal3131@gmail.com>
2020-11-07 23:16:29 -0500

    Function Added

--- ch08-project2-start-master/js/functions.js
+++ ch08-project2-start-master/js/functions.js
@@ -1,1 +1,9 @@
 /* add in your functions here */
+function outputCountryBox(name, continent, cities, photos) {
+	function outputCities(cityarray) {
+		var citieshtml = '';
+		for (let index = 0; index < cityarray.length; index++) {
+			citieshtml += '<li>' + cityarray[index] + '</li>';
+		}
+		return citieshtml;
+	}
